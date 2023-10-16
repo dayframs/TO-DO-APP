80a151e1c7691200b61b812f18d3cc2a9610f65b
dayframs <dayframs09@yahoo.com>
2023-10-16 01:42:23 +0100

    second commit

--- todopage.js
+++ todopage.js
@@ -2,10 +2,7 @@ function displayToDo() {
     let b = resultB.innerHTML;
     let c = localStorage.val;
     let d = JSON.parse(localStorage.val);
-    for (let i = 0; i < val.length; i++){
-        let sn = i + 1;
-        resultB.innerHTML = sn + ' ' + val[i] + ' ' + '<br>';
-    }
+    resultB.innerHTML = d;
 }
 
 displayToDo();
